a2ad83fc0ecd95e79ba486bada3e8f78a11d3c87
diksha002 <diksha.raghoonundun@gmail.com>
2020-03-08 18:32:34 +0000

    test for Input value qu12

--- addTwoInt.py
+++ addTwoInt.py
@@ -3,10 +3,14 @@ def add(x,y):
 	return x+y
 
 if __name__=="__main__":
+	print(sys.argv)
 	if (len(sys.argv) > 3):
 		print("Error! Affichez que 2 variables")
-	elif (len(sys.argv) < 3):
-		print("Error! Affichez 2 variables")
+	elif (len(sys.argv) == 1):
+		print("Error! Affichez")
+          	input("Inserez deux  valeur: ")
+	elif (len(sys.argv) == 2):
+		input("Inserez encore une valeur: ")
 	else:
 		x =int( sys.argv[1] )
 		y =int( sys.argv[2] )
