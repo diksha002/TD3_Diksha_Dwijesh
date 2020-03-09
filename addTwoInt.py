05cfdc3fa9c894c6d299243eee83879f75b942fa
diksha002 <diksha.raghoonundun@gmail.com>
2020-03-09 14:38:56 +0400

    input valeurs manquante

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,17 +1,27 @@
+
 import sys
-def add(x,y):
+def add(x, y):
 	return x+y
 
 if __name__=="__main__":
-	print(sys.argv)
-	if (len(sys.argv) > 3):
-		print("Error! Affichez que 2 variables")
-	elif (len(sys.argv) == 1):
-		print("Error! Affichez")
-          	input("Inserez deux  valeur: ")
-	elif (len(sys.argv) == 2):
-		input("Inserez encore une valeur: ")
+	if ( len(sys.argv) > 3 ):
+		print ("Erreur! Entrez que 2 valeurs")
+	elif ( len(sys.argv) == 1 ):
+		print ("Error! Entrez deux valeurs")
+		x = input("premier valeur: ")
+		y = input("deuxieme valeur: ")
+		x = int(x)
+		y = int(y)
+		print( add(x, y) )
+	elif ( len(sys.argv) == 2 ):
+		print ("Erreur! Entrez encore une valeur")
+		y = input("deuxieme valeur: ")
+		x = int( sys.argv[1] )
+		y = int(y)
+		print( add(x, y) )
 	else:
-		x =int( sys.argv[1] )
-		y =int( sys.argv[2] )
-		print(add(x,y))
+		x = int( sys.argv[1] )
+		y = int( sys.argv[2] )
+		print( add(x, y) )
+
+
