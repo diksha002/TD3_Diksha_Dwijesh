2337c3f87bfa65e05bc0e97b0a6a922003d9c908
dwijesh080200 <dwi.rag123@gmail.com>
2020-03-11 00:04:36 +0400

    final mul main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from addTwoInt import add
+from mulTwoInt import mul
 
 if __name__=="__main__":
 	print("Voulez vous additionner deux valeurs?")
@@ -23,4 +24,25 @@ if __name__=="__main__":
 				y = int( sys.argv[2] )
 				print(add(x,y))
 	else:
-		print("fin du programme")
+		print("Voulez vous multiplier deux valeurs?")
+		print("Inserez oui ou non")
+		a = input()
+		if (a == "oui"):
+			if ( len(sys.argv) > 3 ):
+				print("Erreur! Entrez que deux valeurs!")
+			elif ( len(sys.argv) == 1 ):
+				print("Erreur! Entrez deux valeurs ")
+				a = int(input("premier valeur: "))
+				b = int(input("deuxieme valeur: "))
+				print(mul(a, b))
+			elif ( len(sys.argv) == 2):
+				print("Erreur! Entrez encore une valeur")
+				a = int(input("deuxieme valeur: "))
+				b = int( sys.argv[1] )
+				print(mul(a, b))
+			else:
+				a = int( sys.argv[1] )
+				b = int( sys.argv[2] )
+				print(mul(a, b))
+
+
